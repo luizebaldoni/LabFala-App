39272d7f04af196fbf2114e86c312de3c99483ac
luize Baldoni <oliveira.luize@acad.ufsm.br>
2025-02-23 14:45:41 -0300

    ultima versão do aplicativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     val camerax_version = "1.4.0-alpha02"
     implementation("androidx.camera:camera-core:${camerax_version}")
     implementation("androidx.camera:camera-camera2:${camerax_version}")
--- app/src/main/java/com/example/fono_project/Second.kt
+++ app/src/main/java/com/example/fono_project/Second.kt
@@ -1,5 +1,6 @@
 package com.example.fono_project
 
+// Bibliotecas necessárias para acesso a permissões, manipulação de mídia, interface gráfica e CameraX
 import android.Manifest
 import android.content.ContentValues
 import android.content.pm.PackageManager
@@ -7,14 +8,12 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -28,6 +27,7 @@ import androidx.camera.video.VideoRecordEvent
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
+import com.bumptech.glide.Glide
 import com.example.fono_project.databinding.ActivitySecondBinding
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
@@ -35,12 +35,23 @@ import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+/**
+ * Alias para o listener de luminosidade.
+ * @param luma Valor médio da luminosidade da imagem.
+ */
 typealias LumaListener = (luma: Double) -> Unit
 
+// Activity responsável pela captura de vídeo, manipulação de imagens GIF e análise de luminosidade.
 class Second : AppCompatActivity() {
+    // Vinculação com o layout via View Binding
     private lateinit var viewBinding: ActivitySecondBinding
 
-    // pedindo permissoes
+    /**
+     * Callback para o resultado da solicitação de permissões.
+     * Verifica se todas as permissões foram concedidas e inicia a câmera.
+     * Caso contrário, exibe uma mensagem e finaliza a Activity.
+     */
+
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults:
         IntArray
@@ -60,23 +71,36 @@ class Second : AppCompatActivity() {
         }
     }
 
-    private lateinit var imageView: ImageView
-    private lateinit var nextButton: Button
-    private lateinit var previousButton: Button
-    private var imageList: List<String> = ArrayList()
+    // Declaração de views e variáveis para manipulação de GIFs
+    private lateinit var image_gifs: ImageView
+    private var currentGifIndex = 0
+    private val gifs = listOf(
+        R.drawable.bola,
+        R.drawable.carro,
+        R.drawable.cavalo,
+        R.drawable.bruxa,
+        R.drawable.bolacha
+    )
 
-    private var currentImageIndex = 0
+    // Botões para navegação entre GIFs e variáveis para captura de imagem e vídeo
+    private lateinit var next: ImageView
+    private lateinit var back: ImageView
     private var imageCapture: ImageCapture? = null
     private var videoCapture: VideoCapture<Recorder>? = null
     private var recording: Recording? = null
     private lateinit var cameraExecutor: ExecutorService
 
+    /**
+     * Método chamado na criação da Activity.
+     * Inicializa o layout, solicita permissões necessárias, configura a câmera,
+     * inicializa botões para captura de vídeo e navegação entre GIFs.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        // pedindo permissao p acessar camera:
+        // Verifica se as permissões necessárias já foram concedidas, caso contrário, solicita
         if (allPermissionsGranted()) {
             startCamera()
         } else {
@@ -84,64 +108,57 @@ class Second : AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
             )
         }
-        // definindo botoes de video e foto
-        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+        // Define ação para o botão de captura de vídeo
         viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
         cameraExecutor = Executors.newSingleThreadExecutor()
-    }
 
-    // CAPTURANDO FOTO:
-    private fun takePhoto() {
-        val imageCapture = imageCapture ?: return
-        // criando time e mediastamp.
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-            }
+        // Inicializa views para exibição de GIFs e botões de navegação
+        image_gifs = this.findViewById(R.id.image_gifs)
+        next = this.findViewById(R.id.next)
+        back = this.findViewById(R.id.back)
+
+        // Carrega o primeiro GIF utilizando Glide
+        Glide.with(this).load(gifs[currentGifIndex]).into(image_gifs)
+
+        // Função interna para avançar para o próximo GIF
+        fun nextGif() {
+            currentGifIndex = (currentGifIndex + 1) % gifs.size
+            Glide.with(this).load(gifs[currentGifIndex]).into(image_gifs)
         }
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(
-                contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues
-            )
-            .build()
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun
-                        onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val msg = "Photo capture succeeded: ${output.savedUri}"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                }
-            }
-        )
+        // Define ação para o botão "next"
+        next.setOnClickListener {
+            nextGif()
+        }
+        // Função interna para voltar ao GIF anterior
+        fun previousGif() {
+            currentGifIndex = (currentGifIndex - 1 + gifs.size) % gifs.size
+            Glide.with(this).load(gifs[currentGifIndex]).into(image_gifs)
+        }
+        // Define ação para o botão "back"
+        back.setOnClickListener {
+            previousGif()
+        }
     }
 
-    // CAPTURANDO VIDEO:
+    /**
+     * Captura ou finaliza a gravação de vídeo.
+     * Se já estiver gravando, para a gravação. Caso contrário, inicia uma nova gravação,
+     * configurando o MediaStore para salvar o arquivo de vídeo.
+     */
     private fun captureVideo() {
         val videoCapture = this.videoCapture ?: return
         viewBinding.videoCaptureButton.isEnabled = false
         val curRecording = recording
         if (curRecording != null) {
-            // para de capturar
+            // Para de capturar vídeo
             curRecording.stop()
             recording = null
             return
         }
-        // cria e começar uma nova captura
+        // Cria um nome baseado na data e hora atual
         val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
             .format(System.currentTimeMillis())
+        // Configura os valores para o arquivo de vídeo
         val contentValues = ContentValues().apply {
             put(MediaStore.MediaColumns.DISPLAY_NAME, name)
             put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
@@ -149,32 +166,36 @@ class Second : AppCompatActivity() {
                 put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
             }
         }
-        // salvar no mediastore
+        // Configura as opções de saída para o MediaStore
         val mediaStoreOutputOptions = MediaStoreOutputOptions
             .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
             .setContentValues(contentValues)
             .build()
+        // Inicia a gravação de vídeo com as configurações definidas
         recording = videoCapture.output
             .prepareRecording(this, mediaStoreOutputOptions)
             .apply {
+                // Habilita a captura de áudio se a permissão estiver concedida
                 if (PermissionChecker.checkSelfPermission(
                         this@Second,
                         Manifest.permission.RECORD_AUDIO
                     ) ==
-                    PermissionChecker.PERMISSION_GRANTED // verifica a permissao
+                    PermissionChecker.PERMISSION_GRANTED // Verifica a permissão
                 ) {
-                    withAudioEnabled() // liga audio
+                    withAudioEnabled() // Habilita áudio
                 }
             }
             .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
                 when (recordEvent) {
                     is VideoRecordEvent.Start -> {
+                        // Atualiza o botão para indicar que a gravação está em andamento
                         viewBinding.videoCaptureButton.apply {
                             text = getString(R.string.stop_capture)
                             isEnabled = true
                         }
                     }
                     is VideoRecordEvent.Finalize -> {
+                        // Finaliza a gravação e verifica se houve erro
                         if (!recordEvent.hasError()) {
                             val msg = "Video capture succeeded: " +
                                     "${recordEvent.outputResults.outputUri}"
@@ -189,6 +210,7 @@ class Second : AppCompatActivity() {
                                         "${recordEvent.error}"
                             )
                         }
+                        // Restaura o botão para o estado inicial
                         viewBinding.videoCaptureButton.apply {
                             text = getString(R.string.start_capture)
                             isEnabled = true
@@ -198,14 +220,19 @@ class Second : AppCompatActivity() {
             }
     }
 
-    // LIGA A CAMERA
+    /**
+     * Inicializa e configura a câmera utilizando a biblioteca CameraX.
+     * Configura o preview, captura de imagem, análise de luminosidade e gravação de vídeo.
+     */
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
+            // Obtém o provider para vincular os casos de uso ao ciclo de vida
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Inicializa a captura de imagem
             imageCapture = ImageCapture.Builder().build()
+            // Configura a análise de imagem para cálculo de luminosidade
             val imageAnalyzer = ImageAnalysis.Builder()
                 .build()
                 .also {
@@ -213,23 +240,24 @@ class Second : AppCompatActivity() {
                         Log.d(TAG, "Average luminosity: $luma")
                     })
                 }
-            // Preview
+            // Configura o preview da câmera
             val preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
+            // Configura o gravador para captura de vídeo com a melhor qualidade disponível
             val recorder = Recorder.Builder()
                 .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
                 .build()
             videoCapture = VideoCapture.withOutput(recorder)
 
-            // Select back camera as a default
+            // Seleciona a câmera frontal como padrão
             val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
             try {
-                // Unbind use cases before rebinding
+                // Desvincula quaisquer casos de uso previamente ligados à câmera
                 cameraProvider.unbindAll()
-                // Bind use cases to camera
+                // Vincula os casos de uso (preview, vídeo e análise) ao ciclo de vida
                 cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, videoCapture, imageAnalyzer
                 )
@@ -239,17 +267,29 @@ class Second : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
+    /**
+     * Verifica se todas as permissões necessárias foram concedidas.
+     * @return true se todas as permissões estiverem concedidas, false caso contrário.
+     */
+
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it
         ) == PackageManager.PERMISSION_GRANTED
     }
 
+    /**
+     * Libera recursos ao destruir a Activity.
+     * Encerra o executor da câmera.
+     */
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
     }
 
+    /**
+     * Objeto companion contendo constantes utilizadas na Activity.
+     */
     companion object {
         private const val TAG = "CameraXApp"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
@@ -265,17 +305,31 @@ class Second : AppCompatActivity() {
             }.toTypedArray()
     }
 
+    /**
+     * Classe interna responsável por analisar a luminosidade de uma imagem.
+     * Implementa o analisador para uso com o ImageAnalysis da CameraX.
+     * @param listener Callback que recebe o valor médio da luminosidade.
+     **/
     private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
 
+        /**
+         * Converte um ByteBuffer em um Array de Bytes.
+         * @return Array de bytes representando os dados do buffer.
+         */
         private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
+            rewind()    // Reinicia a posição do buffer para zero
             val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
+            get(data)   // Copia o conteúdo do buffer para o array
+            return data // Retorna o array de bytes
         }
 
+        /**
+         * Analisa a imagem para calcular a luminosidade média.
+         * Converte o buffer da primeira camada da imagem em um array de bytes,
+         * calcula a média dos valores dos pixels e passa o resultado para o listener.
+         * @param image Imagem capturada pela câmera.
+         */
         override fun analyze(image: ImageProxy) {
-
             val buffer = image.planes[0].buffer
             val data = buffer.toByteArray()
             val pixels = data.map { it.toInt() and 0xFF }
